792f5844576a892b97189242efaa2480c0fd50bf
Sam Gillis <sbvb13@gmail.com>
2020-10-09 16:44:12 +0000

    Completed parent class abstract melon
    Co-authored-by: Erin Grady <erinmariegrady@gmail.com>

--- melons.py
+++ melons.py
@@ -1,44 +1,16 @@
 """Classes for melon orders."""
+# Write a parent class for abstract melon
+# Get rid of repetitive code in DomesticMelonOrder and Int'l Melon Order
+class AbstractMelonOrder():
+    """An abstract based class other melon classes inherit from"""
 
-
-class DomesticMelonOrder():
-    """A melon order within the USA."""
-
-    def __init__(self, species, qty):
+    def __init__(self, species, qty, country_code='USA'):
         """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
-        self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
-
-class InternationalMelonOrder():
-    """An international (non-US) melon order."""
-
-    def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
-
+    
         self.species = species
         self.qty = qty
         self.country_code = country_code
         self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -47,11 +19,24 @@ class InternationalMelonOrder():
         total = (1 + self.tax) * self.qty * base_price
 
         return total
-
+     
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
-        self.shipped = True
+        self.shipped = True    
+        
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+       
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    order_type = "international"
+    tax = 0.17
 
     def get_country_code(self):
         """Return the country code."""
